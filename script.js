674fb537b45309778773a5672a6f0e96a88055b8
ehsanmousavif <ehsan.mousavi.f12345@gmail.com>
2025-01-18 16:24:02 +0330

    first commit

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-const showDisplay = document.querySelector(".display");
-const btn = document.querySelectorAll(".show-display");
-const clearAll = document.querySelector(".all-clear");
-const clearLast = document.querySelector(".clear-last");
-const equal = document.querySelector(".calculate");
-//show result
-function showResult(event) {
-  const x = event.target.innerText;
-  if (showDisplay.innerHTML == 0) {
-    showDisplay.innerText = x;
-  } else {
-    showDisplay.innerHTML += x;
-  }
-}
-function calculate() {
-  const result = showDisplay.innerHTML;
-  showDisplay.innerHTML = eval(result);
-}
-function clearNumbers() {
-  if (showDisplay.innerHTML.length <= 2) {
-    showDisplay.innerHTML = 0;
-  } else {
-  return  showDisplay.innerHTML.substring(0, showDisplay.innerHTML.length - 1);
-  }
-}
-function clearAllNumbers(){
-    return showDisplay.innerHTML=0;
-}
-btn.forEach((items) => {
-  return items.addEventListener("click", showResult);
-});
+const clock = document.querySelector(".clock");
 
-equal.addEventListener("click", calculate);
-clearLast.addEventListener("click", clearNumbers);
-clearAll.addEventListener("click", clearAllNumbers);
+function timeHandler() {
+  let date = new Date();
+  let dateTypes = {
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds(),
+  };
+
+
+  const s = dateTypes.seconds < 10 ? `0${dateTypes.seconds}` : `${dateTypes.seconds}`;
+  const m = dateTypes.minutes < 10 ? `0${dateTypes.minutes}` : `${dateTypes.minutes}`;
+  const H = dateTypes.hours < 10 ? `0${dateTypes.hours}` : `${dateTypes.hours}`;
+  const time = `${H}: ${m}: ${s}`;
+  clock.innerText = time;
+}
+setInterval(timeHandler, 1000);
